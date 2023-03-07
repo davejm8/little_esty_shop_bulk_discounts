ff38fe383a403d589d4cd17a752457958f509752
David Marino <davidjmarino8@gmail.com>
2023-03-07 16:33:02 -0500

    Submission ready

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -16,22 +16,15 @@ class Invoice < ApplicationRecord
   end
 
   def total_discounted
-    rev = invoice_items.joins(item: { merchant: :bulk_discounts }).where('invoice_items.quantity >= bulk_discounts.threshold').group('invoice_items.id').select('MAX(invoice_items.unit_price * invoice_items.quantity * bulk_discounts.percentage) as total')
+    rev = invoice_items
+    .joins(item: { merchant: :bulk_discounts })
+    .where('invoice_items.quantity >= bulk_discounts.threshold')
+    .group('invoice_items.id').select('MAX(invoice_items.unit_price * invoice_items.quantity * bulk_discounts.percentage) as total')
     rev.sum(&:total)
-  
   end
 
   def merchant_total_discount_revenue
     converted = total_discounted / 100
     total_revenue - converted
   end
-
-  # def discounted_items(merchant_id)
-  #   invoice_items.joins(:bulk_discounts)
-  #   .where(merchants: {id: merchant_id})
-  #   .where("invoice_items.quantity >= bulk_discounts.threshold")
-  #   .group(:id)
-  #   .select("max(bulk_discounts.percentage) as max_discount, invoice_items.*")
-  #   #select the highest percentage discount that the quantity applies to
-  # end
 end
